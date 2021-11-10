3bb4a3a75412e8b9ce8e7e22908c3e5f9c2415d3
Renjie Deng <dengrenjie31@gmail.com>
2021-11-10 14:17:47 +0800

    Try to use docker to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source .bashrc
-pm2 stop tiebarankgo
-mv /home/ubuntu/application/app/tiebarankgo /home/ubuntu/application/app/tiebarank
-pm2 start tiebarankgo
+docker stop rank
+docker rm rank
+docker rmi dengrenjie31/rank
+docker-compose up -d
