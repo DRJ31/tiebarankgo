630b26b2754e94e93a06754c08ffb91be90f6629
Renjie Deng <dengrenjie31@gmail.com>
2022-08-21 12:10:18 +0800

    CircleCI: Add manifest auto generation
    Signed-off-by: Renjie Deng <dengrenjie31@gmail.com>

---
+++ updateManifest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -v
+
+docker manifest create dengrenjie31/rank \
+	dengrenjie31/rank:amd64 \
+	dengrenjie31/rank:aarch64
+
+docker manifest annotate dengrenjie31/rank:amd64 \
+	--os linux --arch amd64
+
+docker manifest annotate dengrenjie31/rank:aarch64 \
+	--os linux --arch arm64 --variant v8
+
+docker manifest push dengrenjie31/rank
+
