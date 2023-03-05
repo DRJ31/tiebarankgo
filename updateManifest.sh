fb54d96fb58e1c7f7f6624ad743b3db918dc1ffb
Renjie Deng <dengrenjie31@gmail.com>
2023-03-05 15:56:05 +0800

    Update updateManifest.sh
    Signed-off-by: Renjie Deng <dengrenjie31@gmail.com>

--- updateManifest.sh
+++ updateManifest.sh
@@ -8,7 +8,9 @@ docker manifest annotate dengrenjie31/rank:amd64 \
 	--os linux --arch amd64
 
 docker manifest annotate dengrenjie31/rank:aarch64 \
-	--os linux --arch arm64 --variant v8
+	--os linux --arch arm64 
 
 docker manifest push dengrenjie31/rank
+
+docker manifest rm dengrenjie31/rank
 
